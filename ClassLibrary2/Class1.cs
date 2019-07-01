2038edf5b3d2f539b20e9f0905a038fd02c4c60f
byniqing <byniqing@163.com>
2019-07-01 10:53:39 +0800

    init

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassLibrary2
+{
+    /// <summary>
+    /// 记录数据用
+    /// </summary>
+    public class Class1
+    {
+        /*
+         https://www.cnblogs.com/landeanfen/p/4816706.html 
+         */
+    }
+}
